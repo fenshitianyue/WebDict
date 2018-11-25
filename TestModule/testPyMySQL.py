32d08a78a17172a5ccd6cd4ec1261c258f65b666
fenshitianyue <abzanda@163.com>
2018-11-25 23:36:51 +0800

    change some code

--- TestModule/testPyMySQL.py
+++ TestModule/testPyMySQL.py
@@ -2,25 +2,28 @@
 #coding:UTF-8
 
 import pymysql
+import chardet
 
 def manage_english(cursor, buf):
-    sql = "select * from mydict where word = %s" % (buf)
-    try:
-        cursor.execute(sql)
-        if cursor.rowcount == 0:
-            print "find result is Empty"
+    sql = "select * from mydict where word = '%s'" % (buf)
+    cursor.execute(sql)
+    if cursor.rowcount == 0:
+        print "find result is Empty"
+    else:
         results = cursor.fetchall()
         for row in results:
             meaning = row[1]
             print meaning
-    except:
-        print "execute faild"
 
 if __name__ == "__main__":
     db = pymysql.connect("localhost", "root", "nihao", "Dict", charset = "utf8")
     cursor = db.cursor()
     buf = "reinterpret"
-    manage_english(cursor, buf)
+    coding = chardet.detect(buf)
+    if coding['encoding'] == "ascii":
+        manage_english(cursor, buf)
+    else:
+        print "查询词是中文"
 
     db.close()
 
