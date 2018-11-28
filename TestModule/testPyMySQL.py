1bff6ce43063e3fd35388f1c01a74bbe37f7bdbd
fenshitianyue <abzanda@163.com>
2018-11-28 12:37:24 +0800

    add some new test cases

--- TestModule/testPyMySQL.py
+++ TestModule/testPyMySQL.py
@@ -5,23 +5,31 @@ import pymysql
 import chardet
 
 def manage_english(cursor, buf):
-    sql = "select * from mydict where word = '%s'" % (buf)
-    cursor.execute(sql)
-    if cursor.rowcount == 0:
-        print "find result is Empty"
-    else:
-        results = cursor.fetchall()
-        for row in results:
-            meaning = row[1]
-            print meaning
+    try:
+        sql = "select * from mydict where word = '%s'" % (buf)
+        print sql
+        cursor.execute(sql)
+        if cursor.rowcount == 0:
+            print "find result is Empty"
+        else:
+            results = cursor.fetchall()
+            for row in results:
+                meaning = row[1]
+                print meaning
+    except:
+        print "some error!"
+
+def manage_chinese(cursor, buf):
+    pass
 
 if __name__ == "__main__":
     db = pymysql.connect("localhost", "root", "nihao", "Dict", charset = "utf8")
     cursor = db.cursor()
-    buf = "reinterpret"
-    coding = chardet.detect(buf)
+    buf_tmp = "world"
+    buf = [buf_tmp]
+    coding = chardet.detect(buf[0])
     if coding['encoding'] == "ascii":
-        manage_english(cursor, buf)
+        manage_english(cursor, buf[0])
     else:
         print "查询词是中文"
 
