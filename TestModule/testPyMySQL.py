d5b21cb12d2402a20689b08fdacb9eec3785ff33
fenshitianyue <abzanda@163.com>
2018-12-05 15:36:38 +0800

    add chinese search test module

--- TestModule/testPyMySQL.py
+++ TestModule/testPyMySQL.py
@@ -3,6 +3,9 @@
 
 import pymysql
 import chardet
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 def manage_english(cursor, buf):
     try:
@@ -20,18 +23,30 @@ def manage_english(cursor, buf):
         print "some error!"
 
 def manage_chinese(cursor, buf):
-    pass
+    sql = "select * from mydict where match(`explain`) against('%s*' in boolean mode)" %(buf)
+    cursor.execute(sql)
+    if cursor.rowcount == 0:
+        print "find result in empty"
+    else:
+        results = cursor.fetchall()
+        result = ""
+        for row in results:
+            result_line = row[0] + '   ' + row[1] + '\n'
+            result = result + result_line
+    result = result + '\n\n如果对结果不满意，可以尝试搜索含义相类似的词噢'
+    print result
 
 if __name__ == "__main__":
     db = pymysql.connect("localhost", "root", "nihao", "Dict", charset = "utf8")
     cursor = db.cursor()
-    buf_tmp = "world"
+    buf_tmp = "试验"
     buf = [buf_tmp]
     coding = chardet.detect(buf[0])
     if coding['encoding'] == "ascii":
         manage_english(cursor, buf[0])
     else:
-        print "查询词是中文"
+        print "shunt successful"
+        manage_chinese(cursor, buf[0])
 
     db.close()
 
