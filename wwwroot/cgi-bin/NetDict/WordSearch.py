5f48bc7ad53322129ebac071cb0ccbc2e708187a
fenshitianyue <abzanda@163.com>
2018-12-05 15:35:29 +0800

    add chinese search realize code

--- wwwroot/cgi-bin/NetDict/WordSearch.py
+++ wwwroot/cgi-bin/NetDict/WordSearch.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
-# coding:UTF-8
+# -*- coding: utf-8 -*-
 
 import pymysql  # 数据库
 import chardet  # 判断字符集
 # import ctypes  # 调用C静态库接口
 import os # 调用系统服务
-import re # 在中文缓存中使用模糊匹配
+## import re # 在中文缓存中使用模糊匹配
 import string # 字符串操作
 import sys # 调用系统服务
 reload(sys)
@@ -18,7 +18,7 @@ sys.setdefaultencoding('utf8')
 
 # 设置中英文缓存
 EnglishCache = {}
-ChineseCache = {}
+## ChineseCache = {}
 
 def add_english_dict(line):
     list_str = line.split(" ")
@@ -26,15 +26,15 @@ def add_english_dict(line):
     meaning = list_str[1].strip("\n")
     EnglishCache[word] = meaning
 
-def add_chinese_dict(line):
-    list_str = line.split(" ")
-    word = list_str[0].strip("\n")
-    meaning = list_str[1].strip("\n")
-    ChineseCache[word] = meaning
+## def add_chinese_dict(line):
+##     list_str = line.split(" ")
+##     word = list_str[0].strip("\n")
+##     meaning = list_str[1].strip("\n")
+##     ChineseCache[word] = meaning
 
 def read_cache():
     fp_e = open("/root/YAB/web_dict/wwwroot/cgi-bin/NetDict/cache/EnglishCache", "r")
-    fp_c = open("/root/YAB/web_dict/wwwroot/cgi-bin/NetDict/cache/ChineseCache", "r")
+##    fp_c = open("/root/YAB/web_dict/wwwroot/cgi-bin/NetDict/cache/ChineseCache", "r")
 # update english cache
     while True:
         line = fp_e.readline()
@@ -42,15 +42,15 @@ def read_cache():
             add_english_dict(line)
         else:
             break
-# update chinese cache
-    while True:
-        line = fp_c.readline()
-        if line:
-            add_chinese_dict(line)
-        else:
-            break
+## # update chinese cache
+##     while True:
+##         line = fp_c.readline()
+##         if line:
+##             add_chinese_dict(line)
+##         else:
+##             break
     fp_e.close()
-    fp_c.close()
+##    fp_c.close()
 
 def write_english_cache():
     fp = open("/root/YAB/web_dict/wwwroot/cgi-bin/NetDict/cache/EnglishCache", "w")
@@ -66,19 +66,19 @@ def write_english_cache():
         fp.write(word + " " + meaning + "\n")
     fp.close()
 
-def write_chinese_cache():
-    fp = open("cache/ChineseCache", "w")
-    num = len(ChineseCache) - 50
-# 如果缓存中的元素个数超过指定缓存容量，则随机删除超过容量的元素
-    if num > 0:
-        for word in ChineseCache.key():
-            del ChineseCache[word]
-            num = num - 1
-            if num == 0:
-                break
-    for word, meaning in ChineseCache.items():
-        fp.write(word + " " + meaning + "\n")
-    fp.close()
+## def write_chinese_cache():
+##     fp = open("cache/ChineseCache", "w")
+##     num = len(ChineseCache) - 50
+## # 如果缓存中的元素个数超过指定缓存容量，则随机删除超过容量的元素
+##     if num > 0:
+##         for word in ChineseCache.key():
+##             del ChineseCache[word]
+##             num = num - 1
+##             if num == 0:
+##                 break
+##     for word, meaning in ChineseCache.items():
+##         fp.write(word + " " + meaning + "\n")
+##     fp.close()
 
 def Response(resp):
     print "<!DOCTYPE HTML>"
@@ -103,7 +103,7 @@ def Error404():
     Response(error_content)
 
 def FindEmpty():
-    empty_content = "抱歉，未找到查询词~"
+    empty_content = "抱歉，未找到查询词~\n试着换一个近似含义的词搜索。\neg: 试验搜不到，换实验试试~"
     Response(empty_content)
 
 def find_english_cache(buf):
@@ -112,16 +112,16 @@ def find_english_cache(buf):
             return meaning
     return 'null'
 
-def find_chinese_cache(buf):
-    pass
+## def find_chinese_cache(buf):
+##     pass
 
 def update_english_buf(buf, meaning):
     EnglishCache[buf] = meaning
     write_english_cache()
 
-def update_chinese_buf(buf, meaning):
-    ChineseCache[buf] = meaning
-    write_chinese_cache()
+## def update_chinese_buf(buf, meaning):
+##     ChineseCache[buf] = meaning
+##     write_chinese_cache()
 
 def manage_english(cursor, buf):
 # 从英文缓存中查找
@@ -145,20 +145,33 @@ def manage_english(cursor, buf):
             Error404()
 
 def manage_chinese(cursor, buf):
-#     # 从中文缓存中查找
-#     meaning = find_chinese_cache(buf)
-#     if meaning != 'null':
-#         Response(meaning)
-#     else:
-    pass
+    try:
+        Response(buf)
+        sql = "select * from mydict where match(`explain`) against('%s*' in boolean mode)" % (buf)
+        cursor.execute(sql)
+        if cursor.rowcount == 0:
+            # FindEmpty()
+            pass
+        else:
+            results = cursor.fetchall()
+            result = ""
+            for row in results:
+                result_line = row[0] + '    ' + row[1] + '\n'
+                result = result + result_line
+            result = result + '\n\n如果对结果不满意，可以尝试换一个查询词试试。\neg:实验搜不到，换试验试试~'
+            # Response(result)
+            pass
+    except:
+        Error404()
 
 def get_query_string(buf):
-# 从环境变量中读取方法
+    # 从环境变量中读取方法
     method = os.environ["REQUEST_METHOD"]
-# 如果是GET方法，再从环境变量中读取 QUERY_STRING
+    # 如果是GET方法，再从环境变量中读取 QUERY_STRING
     if method.upper() == "GET":
         query_string = os.environ["QUERY_STRING"]
         buf[0] = query_string
+        Response(unicode(query_string[2:], 'utf-8')) #接收到的query_string的编码可能有问题
 # 如果是POST方法，从环境变量中获取content-length，再
 # 根据其数值从标准输入中读取数据
     else:
@@ -191,12 +204,10 @@ if __name__ == "__main__":
 # 进行中英文分流
     coding = chardet.detect(buf[0])
     if coding['encoding'] == "ascii":
-        manage_english(cursor, buf[0])
+        # manage_english(cursor, buf[0])
+        pass
     else:
         manage_chinese(cursor, buf[0])
 # 关闭与数据库的连接
     db.close()
-
-
-
 
