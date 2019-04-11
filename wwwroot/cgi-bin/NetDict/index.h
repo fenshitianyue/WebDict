bf23a0507c8ac5e9cb2ce3b4258c65ba01f5e4cc
fenshitianyue <abzanda@163.com>
2019-04-11 23:17:49 +0800

    update index header code

--- wwwroot/cgi-bin/NetDict/index.h
+++ wwwroot/cgi-bin/NetDict/index.h
@@ -1,60 +1,92 @@
+#pragma once 
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <string>
 #include <unordered_map>
-#include <boost/algorithm/string.hpp> //使用boost库提供的字符串切分
+#include <boost/algorithm/string.hpp>
 #include <cppjieba/Jieba.hpp>
+#include <gflags/gflags.h>
 
-using word_info = word_index_proto::WordInfo;
-using weight = word_index_proto::Weight;
-
-using InvertedList = std::vector<weight>;
-
-//表示分词结果的统计结果
-struct WordCnt{
-  int word_cnt;
-  WordCnt(): word_cnt(0){}
-};
-
-
-class Index{
+/*             util            */
+class DictUtil{
 public:
-  static Index* Instance(){
-    if(_inst == NULL){
-      _inst = new Index();
+  bool Load(const std::string& file_path){
+    std::ifstream file(file_path.c_str());
+    if(!file.is_open()){
+      return false;
     }
-    return _inst;
+    std::string line;
+    while(std::getline(file, line)){
+      set_.insert(line);
+    }
+    file.close();
+    return true;
   }
-  Index();
-
+  bool Find(const std::string& key) const {
+    return set_.find(key) != set_.end();
+  }
 private:
-  //正排索引
-  std::vector<word_info> _sequence_index;
-  //倒排索引
-  std::unordered_map<std::string, InvertedList> _inverted_index;
-  static Index* _inst;
-  //cppjieba::Jieba _jieba;
-public:
-  bool Build(const std::string& file_path);
-  const word_info* GetWordInfo(const uint64_t id) const;
-  const InvertedList* GetInvertedList(const std::string& key) const;
-private:
-  //一些用于构建索引的内部函数
-  const word_info* BuildSequence(const std::string& line);
-  void BuildInverted(const word_info& WordInfo);
-  void SortInvertedList();
+  std::unordered_set<std::string> set_;
 };
 
 class StringUtil{
 public:
-  static void Split(const std::string& input, std::vector<std::string>* output, const std::string split_char){
+  static void Split(const std::string& input, std::vector<std::string>* output, const std::string& split_char){
     boost::split(*output, input, boost::is_any_of(split_char), boost::token_compress_off);
   }
 };
 
-class Search{
+static void InitApp(int argc, char** argv){
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+}
 
+/* end util */
+
+struct WordInfo{
+  int64_t word_id;
+  std::string word;
+  std::string explain;
+  //含义的分词结果
+  std::vector<std::string> cut_result;
+};
+
+struct Weight{
+  int64_t word_id;
+  int32_t weight;
+};
+
+typedef std::vector<Weight> InvertedList;
+
+class Index{
+public:
+  static Index* Instance(){
+    if(inst_ == NULL){
+      inst_ = new Index();
+    }
+    return inst_;
+  }
+  Index();
+  bool Build(const std::string& file_path);
+  bool Save(const std::string& output_path);
+  bool Load(const std::string& output_path);
+  //void PrintForward();
+  //void PrintInverted();
+private:
+  const WordInfo* BuildForward(const std::string& line);  //创建正排索引
+  void SplitExplain(const std::string& explain, WordInfo* word_info); //对单词中文含义进行分词
+  static bool CmpWeight(const Weight& w1, const Weight& w2);
+  static bool EqualWeight(Weight& w1, Weight& w2);
+  int32_t CalcWeight(const std::string& explain, const std::string& word); //计算分词的权重
+  void BuildInverted(const WordInfo& word_info); //创建倒排索引
+  void SortInvertedList(); //对倒排拉链排序
+private:
+  std::vector<WordInfo> forward_index_;
+  std::unordered_map<std::string, InvertedList> inverted_index_;
+  cppjieba::Jieba jieba_;
+  //暂停词
+  DictUtil stop_word_dict_; 
+  static Index* inst_;
 };
 
 
